ae8a4f4732371b37ae4bdab5e303a217134e1f21
Joshua Kerber <jkerber14@comcast.net>
2016-08-09 14:41:37 -0400

    with content

--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -14,10 +14,10 @@ import Post from '../models/post_model';
 export const cleanPosts = (posts, single) => {
   console.log('CLEAN called');
   if (single === true) {
-    return { id: posts._id, title: posts.title, tags: posts.tags };
+    return { id: posts._id, title: posts.title, tags: posts.tags, content: posts.content };
   } else {
     return posts.map(post => {
-      return { id: post._id, title: post.title, tags: post.tags };
+      return { id: post._id, title: post.title, tags: post.tags, content: posts.content };
     });
   }
 };
